2b63bfaa4df09b3dac95bc99deeaf8e6708137d1
Billy Wang <billy.wang.aus@gmail.com>
2021-05-05 23:27:12 +1000

    CyclicRotation

--- BinaryGap.cs
+++ BinaryGap.cs
@@ -7,25 +7,25 @@ namespace CodeTest
     // BinaryGap
     public class BinaryGap
     {
-        static void Main()
+        static void BinaryGapMain()
         {
-            int[] x =  { 
+            int[] x =  {
                 1041,
-                32, 
-                66561, 
-                6291457, 
-                74901729, 
-                805306373, 
-                1376796946, 
-                1073741825, 
-                1610612737 
+                32,
+                66561,
+                6291457,
+                74901729,
+                805306373,
+                1376796946,
+                1073741825,
+                1610612737
             };
 
             for (int i = 0; i < x.Length; i++)
             {
                 int result = solution1(x[i]);
 
-                Console.WriteLine(x[i] + " result: " + result); 
+                Console.WriteLine(x[i] + " result: " + result);
             }
         }
 
@@ -39,30 +39,30 @@ namespace CodeTest
             int startIndex = binary.IndexOf('1', 0);
 
             while (startIndex != -1)
-	        {
+            {
                 int substringStart = startIndex + 1;
 
                 // find next 1 after substringStart
                 int substringEnd = binary.IndexOf('1', substringStart);
 
                 if (substringEnd < 0)
-	            {
+                {
                     break;
-	            }
+                }
 
                 zeroList.Add(binary.Substring(substringStart, substringEnd - substringStart));
 
                 startIndex = binary.IndexOf('1', substringEnd);
-	        }
+            }
 
             if (zeroList.Count > 0)
-	        {
-                 return zeroList.Max(x => x.Length);
-	        }
+            {
+                return zeroList.Max(x => x.Length);
+            }
             else
-	        {
+            {
                 return 0;
-	        }
+            }
         }
 
         // from Github
@@ -81,7 +81,7 @@ namespace CodeTest
             // 1 1 do nothing
             for (int i = 1; i < binaryRepresentation.Length; i++)
             {
-                if (binaryRepresentation[i-1] == '1' && binaryRepresentation[i] == '0')
+                if (binaryRepresentation[i - 1] == '1' && binaryRepresentation[i] == '0')
                 {
                     binaryGapLenght = 1;
                 }
---
+++ CyclicRotation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeTest
+{
+    // BinaryGap
+    public class CyclicRotation
+    {
+        static void Main()
+        {
+            int[] x = { };
+            int k = 1;
+
+            var result = solution1(x, k);
+
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static int[] solution1(int[] A, int K)
+        {
+            if (K == A.Length || A.Length == 0)
+            {
+                return A;
+            }
+
+            int[] currentArray = A;
+
+            for (int i = 0; i < K; i++)
+            {
+                int[] newArray = new int[currentArray.Length];
+                newArray[0] = currentArray.Last();
+                Array.Copy(currentArray, 0, newArray, 1, currentArray.Length - 1);
+                currentArray = newArray;
+            }
+
+            return currentArray;
+        }
+
+        // from Github
+        public static int[] solution2(int[] A, int K)
+        {
+            if (K == A.Length || A.Length == 0)
+            {
+                return A;
+            }
+
+            for (int i = 0; i < K; i++)
+            {
+                int last = A[A.Length - 1];
+                for (int j = A.Length - 1; j > 0; j--)
+                {
+                    A[j] = A[j - 1];
+                }
+                A[0] = last;
+            }
+
+            return A;
+        }
+    }
+}
